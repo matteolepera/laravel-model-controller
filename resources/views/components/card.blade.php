0617a81182fa5af33334e3f8ee619eebaec6cec3
matteolpera <matteo.lepera02@gmail.com>
2026-03-23 16:57:27 +0100

    feat: aggiunto componente card.

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,11 @@
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title">{{ $card_title }}</h5>
+        <p class="card-text">{{ $card_subtitle }}</p>
+        <span>{{ $nazionality }}</span>
+        <span>{{ $year }}</span>
+    </div>
+    <div class="card-footer">
+        <small class="text-body-secondary">{{ $vote }} ⭐️</small>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,21 @@
     <h1>Film</h1>
 
     <ul>
-        @foreach ($movies as $movie)
-            <li>{{ $movie['title'] }}</li>
-        @endforeach
+        <!-- @foreach ($movies as $movie)
+                                <li>{{ $movie['title'] }}</li>
+                            @endforeach -->
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            @foreach ($movies as $movie)
+                <div class="col text-center">
+                    <x-card>
+                        <x-slot:card_title>{{$movie["title"]}}</x-slot:card_title>
+                        <x-slot:card_subtitle>{{$movie["original-title"]}}</x-slot:card_subtitle>
+                        <x-slot:nazionality>{{$movie["nationality"]}}</x-slot:nazionality>
+                        <x-slot:year>{{$movie["date"]}}</x-slot:year>
+                        <x-slot:vote>{{$movie["vote"]}}</x-slot:vote>
+                    </x-card>
+                </div>
+            @endforeach
+        </div>
     </ul>
 @endsection
